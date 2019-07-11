d46130d5ce200d223c5f1ce9a4726e3d215bac17
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 21:52:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,9 @@ def monopoly_with_third_tier
   monopoly = {railroads: {
       pieces: 4,
       names: {},
-      rent_in_dollars: {}
+      rent_in_dollars: {
+        one_piece_owned: 25
+      }
     }
   }
 end
