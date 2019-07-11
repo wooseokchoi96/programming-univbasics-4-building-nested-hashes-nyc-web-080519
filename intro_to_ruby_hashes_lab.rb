20267b24b1de082226c00c552b9340bc1824bda4
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 21:56:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,9 +21,15 @@ def monopoly_with_third_tier
         reading_railroad: {
           mortgage_value: '$100'
         },
-        pennsylvania_railroad: {},
-        b_and_o_railraod: {},
-        shortline: {}
+        pennsylvania_railroad: {
+          mortgage_value: '$200'
+        },
+        b_and_o_railraod: {
+          mortgage_value: '$400'
+        },
+        shortline: {
+          mortgage_value: '$800'
+        }
       },
       rent_in_dollars: {
         one_piece_owned: 25,
