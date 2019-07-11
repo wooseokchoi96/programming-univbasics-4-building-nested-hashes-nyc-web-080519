9cddcf0efc1bbf7a22f772ed229958c09e33b7b9
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 21:54:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
       names: {
         reading_railroad: {},
         pennsylvania_railroad: {},
-        
+        b_and_o_
       },
       rent_in_dollars: {
         one_piece_owned: 25,
